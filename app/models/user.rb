a10935f5fcb8d74e1d4b2d1a9f95cf2ca776e979
Anton Naumenkov <a.naumenkov@mobidev.biz>
2021-12-10 10:42:01 +0200

    user can update profile and chagne pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
+  
   validates :first_name, :last_name, :email, presence: true
   validates :password, presence: true, confirmation: true
 
